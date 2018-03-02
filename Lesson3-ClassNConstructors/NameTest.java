50fe67f27d27acffb4632c62c6ee6284416039ca
cod3breaker666 <cod3breaker666@gmail.com>
2018-03-01 19:15:28 -0600

    Lesson 3 - Class and Constuctors

---
+++ Lesson3-ClassNConstructors/NameTest.java
@@ -0,0 +1,13 @@
+public class NameTest
+{
+	public static void main(String[] args)
+	{
+	// instantiation
+	Name myName = new Name("Aqeel", "ahmed", "Khan");
+	Name yourName = new Name("Humnah","Khan");
+	Name someName;
+	System.out.println("mName:  " + myName.toString());
+	System.out.println("yourName:  " + yourName.toString());	
+	
+	}
+}
