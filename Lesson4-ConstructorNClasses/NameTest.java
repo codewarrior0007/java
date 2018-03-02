dc926dfa4948093facd34892fe91561eda68f0df
cod3breaker666 <cod3breaker666@gmail.com>
2018-03-01 19:24:04 -0600

    Class4-ConstrutorNClasses

---
+++ Lesson4-ConstructorNClasses/NameTest.java
@@ -0,0 +1,13 @@
+public class NameTest
+{
+	public static void main(String[] args)
+	{
+	// instantiation
+	Name myName = new Name("Joe", "K", "Light");
+	Name yourName = new Name("Jane","Smith");
+	Name someName;
+	System.out.println("mName:  " + myName.toString());
+	System.out.println("yourName:  " + yourName.toString());	
+	
+	}
+}
